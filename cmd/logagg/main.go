bdd3c5eef36072cbf48807fb8302f7f61c16609c
Yamitsuki <anyuenight@gmail.com>
2026-01-16 14:38:11 +0900

    ✨ feat: HTTP APIサーバーを実装

---
+++ cmd/logagg/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Yamituki/go-review-logagg/internal/server"
+)
+
+func main() {
+	srv := server.NewServer(":8080")
+	log.Println("サーバーを起動しています: http://localhost:8080")
+	if err := srv.Start(); err != nil {
+		log.Fatalf("サーバーの起動に失敗: %v", err)
+	}
+}
