f9ed16c50126d2e070b794f7b79f9c7b9a38b4fd
Yamitsuki <anyuenight@gmail.com>
2026-01-16 14:42:51 +0900

    🐛 fix: SetupRoutes呼び出しを追加

--- cmd/logagg/main.go
+++ cmd/logagg/main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	srv := server.NewServer(":8080")
+	srv.SetupRoutes()
 	log.Println("サーバーを起動しています: http://localhost:8080")
 	if err := srv.Start(); err != nil {
 		log.Fatalf("サーバーの起動に失敗: %v", err)
